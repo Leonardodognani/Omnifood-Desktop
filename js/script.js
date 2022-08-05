31fa54cedad1f7ec53ff97f9bcb42a16b5a4f2fd
Leonardo Dognani <roberto.leonardo@venturus.org.br>
2022-08-05 16:18:16 -0300

    fix mobile, smooth scrollig, sticky nav bar, browser suport & safari

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 // Exemple of Js in HTML and CSS, changing the Date of the copyright in the HTML
 // Set current year
+
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
@@ -9,6 +10,7 @@ yearEl.textContent = currentYear;
 
 
 // Making mobile navigation work
+
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener('click', function() {
@@ -17,6 +19,62 @@ btnNavEl.addEventListener('click', function() {
 //////////////////////////////////////////////////////
 
 
+
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href ==="#") window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+
+    // Scroll to other links
+    if(href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth"});
+    }
+
+
+    // Close mobile navigation
+    if(link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+
+
+// Sticky Navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(function(entries) {
+  const ent = entries[0];
+
+    if(ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if(ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  }, 
+ {
+  // In the viewport
+  root: null,
+  threshold: 0,
+  rootMargin: "-80px",
+});
+obs.observe(sectionHeroEl);
 
 
 
